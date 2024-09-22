ba6ea04b74520668bed45929c0a37465eb9e4143
SSW1113 <ysshin1016@gmail.com>
2024-09-23 05:42:17 +0900

    선수 강화 API

--- src/routers/team.router.js
+++ src/routers/team.router.js
@@ -88,4 +88,92 @@ router.post('/gacha/:userId', authMiddleware, async (req, res, next) => {
   }
 });
 
-router.update('/upgrade', async (req, res, next) => {});
+// 강화 성공 여부 결정하는 함수
+function isUpgradeSuccessed(upgrade) {
+  const upgradeChances = [100, 81, 64, 50, 26, 15, 7, 4, 2, 1];
+
+  const successChance = upgradeChances[upgrade];
+  const random = Math.random() * 100;
+  return random < successChance;
+}
+
+// 선수 강화 API
+router.update('/upgrade/:userId/:playerId', async (req, res, next) => {
+  const { userId, playerId } = req.params;
+
+  try {
+    // 유저에게 해당 선수가 있는지 확인
+    const userPlayer = await prisma.userPlayer.findFirst({
+      where: {
+        userId: +userId,
+        playerId: +playerId,
+      },
+      include: {
+        player: true,
+      },
+    });
+
+    // 해당 선수가 없는 경우
+    if (!userPlayer) {
+      return res.status(404).json({ error: '해당 선수를 보유하고 있지 않습니다.' });
+    }
+
+    // 이미 최대 강화(10강)인 경우
+    if (userPlayer.upgrade >= 10) {
+      return res.status(400).json({ error: '이미 최대 강화에 도달하였습니다.' });
+    }
+
+    const requiredCount = userPlayer.upgrade + 1; // 강화에 필요한 카드 수 = 강화치 + 1
+
+    // 강화 재료 부족한 경우
+    if (userPlayer.count < requiredCount) {
+      return res
+        .status(400)
+        .json({ error: `강화에 필요한 카드 수가 모자랍니다. (보유: ${userPlayer.count}, 필요: ${requiredCount})` });
+    }
+
+    // 강화 성공 여부
+    const success = isUpgradeSuccessed(userPlayer.upgrade);
+
+    if (success) {
+      // 성공
+      const updatedUserPlayer = await prisma.userPlayer.update({
+        where: {
+          Id: userPlayer.Id,
+        },
+        data: {
+          upgrade: userPlayer.upgrade + 1,
+          count: userPlayer.count - requiredCount,
+          updatedAt: new Date(),
+        },
+      });
+
+      // 선수 스탯 강화하는 부분인데, 선수 테이블을 건드려버리기 때문에 주석처리함. 아마 게임 로직 부분에서 구현해야 할 듯
+      // const updatedUserPlayerStat = await prisma.player.update({
+      //   where: {playerId : userPlayer.playerId},
+      //   data: {
+      //     speed: userPlayer.player.speed + 1,
+      //     finishing: userPlayer.player.finishing + 1,
+      //     pass: userPlayer.player.pass + 1,
+      //     defense: userPlayer.player.defense + 1,
+      //     stamina: userPlayer.player.stamina + 1,
+      //   }
+      // });
+
+      return res.status(200).json({ message: '강화에 성공하였습니다!' });
+    } else {
+      // 실패
+      const updatedUserPlayer = await prisma.userPlayer.update({
+        where: { Id: userPlayer.Id },
+        data: {
+          count: userPlayer.count - requiredCount,
+          updatedAt: new Date(),
+        },
+      });
+
+      return res.status(200).json({ message: '강화에 실패하였습니다..' });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
