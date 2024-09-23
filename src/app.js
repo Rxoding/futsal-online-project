91d8e96d055ac5cd440f07519a45e3cc9e163b5c
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-09-23 10:33:26 +0900

    test merge 1차 수정

--- src/app.js
+++ src/app.js
@@ -2,44 +2,24 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 
 import LogMiddleware from './middlewares/log.middleware.js';
-import TeamRouter from './routes/team.router.js';
-import userRouter from './routes/user.router.js';
-import GameRouter from "./routers/game.router.js";
+import PlayerRouter from './routers/player.router.js';
+import gameRouter from './routers/game.router.js';
+import teamRouter from './routers/team.router.js';
+import userRouter from './routers/user.router.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-//dotenv.config();
+dotenv.config();
 
 const app = express();
-const PORT = 3018;
+const PORT = 3029;
 
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [userRouter, TeamRouter, GameRouter]);
+app.use('/api', [userRouter, teamRouter, PlayerRouter, gameRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
 });
-// import PlayerRouter from './routers/player.router.js';
-// import LogMiddleware from './middlewares/log.middleware.js';
-// import UserRouter from './routers/user.router.js';
-// import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = 3029;
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use('/api', [PlayerRouter, UserRouter]);
-// app.use(LogMiddleware);
-// app.use(ErrorHandlingMiddleware);
-
-// app.listen(PORT, () => {
-//   console.log(PORT, '포트로 서버가 열렸어요!');
-// });
-
