0e0d5f87d898e56e6e09a0f077a06e597df307d2
UIHyeonLEE <113573945+UIHyeonLEE@users.noreply.github.com>
2024-09-23 10:15:01 +0900

    GameRouter추가

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import LogMiddleware from './middlewares/log.middleware.js';
 import TeamRouter from './routes/team.router.js';
 import userRouter from './routes/user.router.js';
+import GameRouter from "./routers/game.router.js";
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 //import dotenv from 'dotenv';
 
@@ -15,7 +16,7 @@ const PORT = 3018;
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [userRouter, TeamRouter]);
+app.use('/api', [userRouter, TeamRouter, GameRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
