1375b738ecb295764a5700bbc59534e92cd84523
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-09-23 14:58:32 +0900

    게임시작 api삭제

--- src/routers/player.router.js
+++ src/routers/player.router.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { prisma } from '../utils/prisma/index.js';
+import express from "express";
+import { prisma } from "../utils/prisma/index.js";
 
 const router = express.Router();
 
 // 선수 목록 조회 API
-router.get('/player', async (req, res, next) => {
+router.get("/player", async (req, res, next) => {
   try {
     const playerList = await prisma.player.findMany({
       select: {
@@ -25,7 +25,7 @@ router.get('/player', async (req, res, next) => {
 });
 
 // 선수 상세 조회 API
-router.get('/player/:playerId', async (req, res, next) => {
+router.get("/player/:playerId", async (req, res, next) => {
   const playerId = req.params.playerId;
   try {
     const playerInfo = await prisma.player.findUnique({
@@ -42,18 +42,6 @@ router.get('/player/:playerId', async (req, res, next) => {
       },
     });
     return res.status(201).json({ playerInfo });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 게임 시작 API
-router.post("/games/play", async (req, res, next) => {
-  const { teamAIds, teamBIds, teamAName, teamBName } = req.body;
-
-  try {
-    const result = await startGame(teamAIds, teamBIds, teamAName, teamBName);
-    return res.status(200).json(result);
   } catch (err) {
     next(err);
   }
