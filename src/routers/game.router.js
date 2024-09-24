73b2b7be89a816e6e441f07a580aca521eef589b
UIHyeonLEE <113573945+UIHyeonLEE@users.noreply.github.com>
2024-09-24 10:42:56 +0900

    update 1

--- src/routers/game.router.js
+++ src/routers/game.router.js
@@ -22,12 +22,12 @@ router.post('/playgame', authMiddleware, async (req, res, next) => {
     const { accountId } = user;
 
     // 로스터 조회
+
     const rosterA = await prisma.userPlayer.findMany({
       where: { teamId: teamAId, userId: userId },
       select: { playerId: true },
     });
     console.log('로스터 A:', rosterA);
-
     const rosterB = await prisma.userPlayer.findMany({
       where: { teamId: teamBId, userId: userId },
       select: { playerId: true },
